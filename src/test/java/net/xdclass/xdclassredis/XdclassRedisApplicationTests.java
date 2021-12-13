cc728a2aa89aeb0a3d43b5ef8e0a950f9540052f
likf <likfedu@163.com>
2021-12-13 13:19:50 +0800

    init

---
+++ src/test/java/net/xdclass/xdclassredis/XdclassRedisApplicationTests.java
@@ -0,0 +1,40 @@
+package net.xdclass.xdclassredis;
+
+import net.xdclass.xdclassredis.model.UserDO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+@SpringBootTest
+class XdclassRedisApplicationTests {
+
+	@Autowired
+	private RedisTemplate redisTemplate;
+
+	@Test
+	void testStringSet() {
+		ValueOperations valueOperations = redisTemplate.opsForValue();
+		valueOperations.set("name", "xdclass.net");
+	}
+
+
+	@Test
+	void testStringGet() {
+		ValueOperations valueOperations = redisTemplate.opsForValue();
+		System.out.println((String) valueOperations.get("name"));
+	}
+
+	@Test
+	public void testSeria(){
+
+		UserDO userDO = new UserDO();
+		userDO.setId(1);
+		userDO.setName("xd");
+		userDO.setPwd("224336");
+
+		redisTemplate.opsForValue().set("user", userDO);
+
+	}
+}
