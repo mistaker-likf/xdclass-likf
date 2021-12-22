99b3ef7035a2bb567a32c95d5b3c09b0ce6cc61e
likf <likfedu@163.com>
2021-12-22 18:06:37 +0800

    SpringCache环境准备

--- src/main/java/net/xdclass/xdclassredis/XdclassRedisApplication.java
+++ src/main/java/net/xdclass/xdclassredis/XdclassRedisApplication.java
@@ -2,8 +2,10 @@ package net.xdclass.xdclassredis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class XdclassRedisApplication {
 
 	public static void main(String[] args) {
